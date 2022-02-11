493c7d8a5f153311e0c70cd14998a0f7b61d101a
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2022-02-11 12:12:15 -0800

    add the TapDetail component

---
+++ src/components/TapDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TapDetail(props){
+
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+
+export default TapDetail;
